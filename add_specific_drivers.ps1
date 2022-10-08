48db982f86eed3711ac9c39ecf86dd2eca4312b1
twigglits <jnaude60512@gmail.com>
2022-10-09 00:56:10 +0200

    removed aliases in specific drivers script

--- add_specific_drivers.ps1
+++ add_specific_drivers.ps1
@@ -5,7 +5,7 @@ $balena="drivers\balenaEtcher-Setup-1.7.9.exe"
 Start-Process -FilePath $balena -ArgumentList "/S /v /qn" -passthru
 
 # Nvidia Driver
-wget -O nvidia_installer.exe https://us.download.nvidia.com/Windows/517.48/517.48-desktop-win10-win11-64bit-international-dch-whql.exe
-mv nvidia_installer.exe drivers\nvidia_installer.exe
+Invoke-WebRequest -O nvidia_installer.exe https://us.download.nvidia.com/Windows/517.48/517.48-desktop-win10-win11-64bit-international-dch-whql.exe
+Move-Item nvidia_installer.exe drivers\nvidia_installer.exe
 $nvidia = "drivers\nvidia_installer.exe"
 Start-Process -FilePath $nvidia -ArgumentList "/S /v /qn" -passthru
