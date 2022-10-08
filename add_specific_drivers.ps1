96cf759bf1adc7d6d10f744d699168d910622f63
twigglits <jnaude60512@gmail.com>
2022-10-09 00:54:17 +0200

    wip for adding specific drivers to fresh windows 10 install

--- add_specific_drivers.ps1
+++ add_specific_drivers.ps1
@@ -1,9 +1,10 @@
+# Balena Etcher
 7z x drivers.7z -y
 start-sleep -Seconds 5
 $balena="drivers\balenaEtcher-Setup-1.7.9.exe"
 Start-Process -FilePath $balena -ArgumentList "/S /v /qn" -passthru
 
-# Add Nvidia Driver
+# Nvidia Driver
 wget -O nvidia_installer.exe https://us.download.nvidia.com/Windows/517.48/517.48-desktop-win10-win11-64bit-international-dch-whql.exe
 mv nvidia_installer.exe drivers\nvidia_installer.exe
 $nvidia = "drivers\nvidia_installer.exe"
