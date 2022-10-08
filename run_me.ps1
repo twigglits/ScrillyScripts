2989529c9030bf1ab9d611e029da2d40f04b08de
twigglits <jnaude60512@gmail.com>
2022-10-09 01:07:08 +0200

    removed more aliases

--- run_me.ps1
+++ run_me.ps1
@@ -1,9 +1,9 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 choco install git -y
 choco upgrade all
 .\git_script.sh
-cd Windows10Debloater
+Set-Location Windows10Debloater
 .\Windows10SysPrepDebloater.ps1 -y
-cd ..
+Set-Location ..
 .\Disable_Windows_Defender_on_Windows_10.bat -y
