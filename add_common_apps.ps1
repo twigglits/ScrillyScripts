8862711b2397bf9be738c0c8aa14fd0be8974886
twigglits <jnaude60512@gmail.com>
2022-10-08 23:52:18 +0200

    changed script to give super user privelleges

--- add_common_apps.ps1
+++ add_common_apps.ps1
@@ -1,10 +1,12 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+PowerShell -Command "Set-ExecutionPolicy RemoteSigned -scope Process; [Net.ServicePointManager]::SecurityProtocol = 'Tls12'; iwr -useb https://raw.githubusercontent.com/gerardog/gsudo/master/installgsudo.ps1 | iex"
+# Added neofetch install via scoop package manager, because it's really cool ;)
+iwr -useb get.scoop.sh | iex
+scoop update
+scoop install sudo
+scoop install gsudo
+scoop install neofetch 
+scoop update neofetch
 
-choco install spotify -y
-choco install discord -y
-choco install googlechrome -y
-choco install steam-client -y
-choco install qbittorrent -y
-choco install vscode -y
-choco install 7zip -y
-choco upgrade all -y
+# Specify the apps you want to install here:
+gsudo -n -w powershell ./apps_list.ps1
---
+++ apps_list.ps1
@@ -0,0 +1,10 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco install gsudo -y
+choco install spotify -y 
+choco install discord -y
+choco install googlechrome -y
+choco install steam-client -y
+choco install qbittorrent -y
+choco install vscode -y
+choco install 7zip -y
+choco upgrade all -y
