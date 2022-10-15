5c95f943ba9ef1cb27bb3bea0f88978c9158e152
twigglits <jnaude60512@gmail.com>
2022-10-15 16:17:06 +0200

    added LAN driver mark2 version

--- add_specific_drivers.ps1
+++ add_specific_drivers.ps1
@@ -8,7 +8,7 @@
 $folders = @()  # Intialization of empty array
 
 # Installers
-$files =  @("nvidia.exe" , "LAN.zip" , "chipset.zip")   # rename this to installers
+$files =  @("nvidia.exe" , "LAN.zip" , "chipset.zip")   # rename this to installers   name of compressed folders..., vs names of actual extracted installers
 
 foreach ($file in $files)
 {
@@ -16,27 +16,32 @@ foreach ($file in $files)
 }
 # First folder is:
 Write-Output $folders[0]
-# Second folder is:
+# # Second folder is:
 Write-Output $folders[1]
-# Third folder is:
+# # Third folder is:
 Write-Output $folders[2]
 
 # Nvidia Driver
-# Invoke-WebRequest -O $files[0] https://us.download.nvidia.com/Windows/517.48/517.48-desktop-win10-win11-64bit-international-dch-whql.exe
-# Move-Item $files[0] drivers\$files[0]
-# $nvidia = "drivers\"+$files[0]
-# Write-Output $nvidia
-# Start-Process -FilePath $nvidia -ArgumentList "/S /v /qn" -passthru
+$dir = "drivers\" + $folders[0] + "\"
+$path = $dir + $files[0]
+new-item -type directory -path $dir -Force
+Invoke-WebRequest https://us.download.nvidia.com/Windows/517.48/517.48-desktop-win10-win11-64bit-international-dch-whql.exe 
+Start-Process -FilePath $path -ArgumentList "/S /v /qn" -passthru
 
-# # LAN driver
-# Invoke-WebRequest -O $files[1] https://dlcdnets.asus.com/pub/ASUS/lan/LAN_V20_1_2019_0_WHQL.zip
-# 7z x $files[1] -y    # if has .zip at the end then do extraction step..... future improvements
-# Move-Item LAN_V20.1.2019.0_WHQL\ drivers\$files[1]
-# $LAN = "drivers\"+ $files[1] + "\AsusSetup.exe"
-# Write-Output $LAN
-# Start-Process -FilePath $LAN -ArgumentList "/S /v /qn" -passthru
+# LAN Driver 
+$dir = "drivers\" + $folders[1] + "\"
+$path = $dir + $files[1]
+new-item -type directory -path $dir -Force
+Invoke-WebRequest https://dlcdnets.asus.com/pub/ASUS/lan/LAN_V20_1_2019_0_WHQL.zip -OutFile $path
+7z x $path -y
+Move-Item 'LAN_V20.1.2019.0_WHQL' 'drivers\LAN\'
+### Else run file directly 
+Start-Process -FilePath 'drivers\LAN\LAN_V20.1.2019.0_WHQL\AsusSetup.exe' -ArgumentList "/S /v /qn" -passthru
 
 # Chipset
-# Invoke-WebRequest -O $files[2] https://dlcdnets.asus.com/pub/ASUS/misc/utils/MEI_Consumer_V11051189.zip   # truncate get zip, exe away and just leave path
-# 7z x $files[2] -ochipset -y 
-# Move-Item <folder name here>\ drivers\$files[2]
+# $dir = "drivers\" + $folders[2] + "\"
+# $path = $dir + $files[2]
+# new-item -type directory -path $dir -Force
+# Invoke-WebRequest https://dlcdnets.asus.com/pub/ASUS/misc/utils/MEI_Consumer_V11051189.zip -OutFile $path # truncate get zip, exe away and just leave path
+# 7z x $files[2] -y
+# # Move-Item <folder name here>\ drivers\$files[2]
