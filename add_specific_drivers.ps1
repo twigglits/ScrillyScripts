a476426c77f1c182f6a7d5fd2e453686a16d830a
twigglits <jnaude60512@gmail.com>
2022-10-16 18:00:00 +0200

    simplified script to extract to specific folder

--- add_specific_drivers.ps1
+++ add_specific_drivers.ps1
@@ -22,20 +22,19 @@ new-item -type directory -path $dir -Force
 Invoke-WebRequest https://us.download.nvidia.com/Windows/517.48/517.48-desktop-win10-win11-64bit-international-dch-whql.exe -OutFile $path
 Start-Process -FilePath $path -ArgumentList "/S /v /qn" -passthru
 
-# LAN Driver 
+# # LAN Driver 
 $dir = "drivers\" + $folders[1] + "\"
 $path = $dir + $files[1]
 new-item -type directory -path $dir -Force
 Invoke-WebRequest https://dlcdnets.asus.com/pub/ASUS/lan/LAN_V20_1_2019_0_WHQL.zip -OutFile $path
-7z x $path -y
-Move-Item 'LAN_V20.1.2019.0_WHQL' 'drivers\LAN\'
-### Else run file directly 
+# # extract archive and move into respective sub folder:
+7z e $path -odrivers\LAN -y
 Start-Process -FilePath 'drivers\LAN\LAN_V20.1.2019.0_WHQL\AsusSetup.exe' -ArgumentList "/S /v /qn" -passthru
 
 # Chipset
-# $dir = "drivers\" + $folders[2] + "\"
-# $path = $dir + $files[2]
-# new-item -type directory -path $dir -Force
-# Invoke-WebRequest https://dlcdnets.asus.com/pub/ASUS/misc/utils/MEI_Consumer_V11051189.zip -OutFile $path # truncate get zip, exe away and just leave path
-# 7z x $files[2] -y
-# # Move-Item <folder name here>\ drivers\$files[2]
+$dir = "drivers\" + $folders[2] + "\"  #folder doesn't have \ sign so then...
+$path = $dir + $files[2]
+new-item -type directory -path $dir -Force
+Invoke-WebRequest https://dlcdnets.asus.com/pub/ASUS/misc/utils/MEI_Consumer_V11051189.zip -OutFile $path # truncate get zip, exe away and just leave path
+7z e $path -odrivers\chipset -y
+Start-Process -FilePath 'drivers\chipset\AsusSetup.exe' -ArgumentList "/S /v /qn" -passthru
