588cdc37665aaa19fc8e56cb4056f87f85d46e7c
twigglits <jnaude60512@gmail.com>
2022-10-16 20:32:34 +0200

    added e2e script

---
+++ e2e.ps1
@@ -0,0 +1,12 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+# Add common apps
+.\add_common_apps.ps1
+# Add specific drivers from specific sources (This is unique for each user and you should not use the defaults set `add_specific_drivers.ps1`)
+.\add_specific_drivers.ps1
+# Disable windows mouse "precision" drivers
+.\toggle_mouse_acc.ps1
+# Debloat windows 10
+.\debloat.ps1
+# Disable windows defender
+.\disable_defender.ps1
+
